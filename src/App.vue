9f374318d412bccf30a0056104ae11ca1460e8d3
李志琪 <2672566020@qq.com>
2022-05-09 06:26:14 +0800

    ss

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,46 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="amount-input">
+    <a-input-number
+      v-model="value"
+      :parser="parser"
+      :formatter="formatNumber"
+      style="width: 100%"
+    />
   </div>
 </template>
-
 <script>
 export default {
-  name: 'App',
+  data() {
+    return {
+      value: ""
+    };
+  },
+  methods: {
+    parser(str) {
+      if (Number(str)) {
+        console.log(str);
+        return str.replace(/^(\d+)(\.\d{0,2})?/g, function(str, p1, p2) {
+          if (!p2) {
+            return `${p1}.00`;
+          } else if (p2.length === 1) {
+            return `${p1}.${p2}0`;
+          } else {
+            return `${p1}.${p2}`;
+          }
+        });
+      }
+      return "";
+    },
+    formatNumber(value) {
+      console.log("formatNumber:", value);
+      return value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="css">
+.amount-input {
+  width: 500px;
+  margin: 100px auto;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
+import Vue from "vue";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+import App from "./App";
+
+Vue.use(Antd);
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>',
-});
+  render: h => h(App)
+}).$mount("#app");
